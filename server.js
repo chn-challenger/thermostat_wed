9150d1c9b6d59ea5ca53a7c73ddd6af68c5d84e7
chn.challenger <chn.challenger@gmail.com>
2015-10-01 23:52:07 +0100

    working

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var root = __dirname;
+var path = require('path');
+
+app.use(express.static(root + '/src'));
+app.use('/src', express.static(path.join(root, 'src')));
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(root + '/index.html'));
+});
+
+
+app.get('/example', function(req, res) {
+  res.sendFile(path.join(root + '/example.html'));
+});
+
+app.get('/geo', function(req, res) {
+  res.sendFile(path.join(root + '/geolocation.html'));
+});
+
+var server = app.listen(9292, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
--- src/App.js
+++ src/App.js
@@ -26,4 +26,50 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("PowerSaver").addEventListener("click", function() {
     userInteract(thermostat.switchPowerSaving());
   });
+
+  var urlCity = window.location.search.replace('?city=', '')|| 'london';
+  // var weathertemp;
+  // var weathername;
+  // var pos;
+
+  $.ajax('http://api.openweathermap.org/data/2.5/weather?q={' + urlCity + '}', {
+    success: function(data) {
+      // console.log(data);
+      var weathertemp = (data.main.temp - 273.15).toFixed(1);
+      var weathername = data.name;
+      var lati = data.coord.lat;
+      var long = data.coord.lon;
+      var pos = {lat: lati, lng: long};
+      // console.log(lati);
+      // console.log(long);
+
+      var map1 = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: lati, lng: long},
+        zoom: 7
+      });
+
+        var infoWindow = new google.maps.InfoWindow({map: map1});
+        infoWindow.setPosition({lat: lati, lng: long});
+        infoWindow.setContent('Found');
+        map1.setCenter(pos);
+
+
+      // console.log(weathertemp);
+      // console.log(weathername);
+      $('#locationTemp').html(weathertemp);
+      $('#location').html(weathername);
+    }
+  });
+
+  // console.log(pos);
+
+  // $.ajax('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyA6H-yp5yYrieFg7osZBAoJLRvQGNe0xhs', {
+  //   success: function(data2) {
+  //     console.log(data2);
+  //   }
+  // });
+
+
+
+
 });
